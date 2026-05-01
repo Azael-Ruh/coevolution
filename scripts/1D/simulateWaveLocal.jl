567d51b46f7b1507cc6ef0906bcf98c8aad0cf15
Azael-Ruh <maxzyas@gmail.com>
2026-05-01 11:20:49 +0200

    Local modification

--- scripts/1D/simulateWaveLocal.jl
+++ scripts/1D/simulateWaveLocal.jl
@@ -1,19 +1,19 @@
 include("../../code/simulate/coevolution1DSimulationTools.jl")
 
-r = 5
-R0 = 1.5
-mutationRate = 0.2
+r = 40
+R0 = 1.3
+mutationRate = 0.18
 xmax::Int64 = 2000
-Nh::Int64 = 1e6
+Nh::Int64 = 1e7
 
-nonLocalJump = 40
+nonLocalJump = 0
 nonLocalMutProb = 1e-6
-localKernel = Normal(0,2)
-mutationKernel = Normal(0,2) # piecewiseKernel("piecewise", nonLocalMutProb, nonLocalJump, localKernel)
+localKernel = Normal(0,1)
+mutationKernel = Normal(0,1) # piecewiseKernel("piecewise", nonLocalMutProb, nonLocalJump, localKernel)
 
 (nx0, hx0, x) = getInitialCondition("steadyState", R0, r, mutationRate, mutationKernel, Nh, xmax)
 
-tmax = 800
+tmax = 4000
 dt = 0.1
 dtSampling = 1
 
